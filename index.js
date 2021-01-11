f44bedf30743340a1d2731c14a1bcec30c90306c
Serge Modin <modinsv@yandex.ru>
2021-01-11 21:44:18 +0300

    Responding with HTML

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const http = require("http");
+const fs = require("fs");
+const homePage = fs.readFileSync("index.html");
+const aboutPage = fs.readFileSync("about.html");
+const contactPage = fs.readFileSync("contact.html");
+const notFoundPage = fs.readFileSync("notfound.html");
+
 const server = http.createServer((req, res) => {
-  if (req.url === "/about") res.end("The about page");
-  else if (req.url === "/contact") res.end("The contact page");
-  else if (req.url === "/") res.end("The home page");
+  if (req.url === "/about") res.end(aboutPage);
+  else if (req.url === "/contact") res.end(contactPage);
+  else if (req.url === "/") res.end(homePage);
   else {
     res.writeHead(404);
-    res.end("page not found");
+    res.end(notFoundPage);
   }
-
 });
 server.listen(3000);
