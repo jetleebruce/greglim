ee9e1f0bcd18dc55e4247ac84e94fab85996f723
Serge Modin <modinsv@yandex.ru>
2021-01-12 16:00:40 +0300

    Added EJS
    We refactored our code to use the EJS templating engine to dynamically render HTML pages. We called res.render to dynamically render a view with some variables. To resolve the problem of repeating code in multiple pagesfor e.g. navigation bar, we used EJS layouts.

--- index.js
+++ index.js
@@ -7,20 +7,22 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-app.get('/',(req,res)=>{
-res.sendFile(path.resolve(__dirname, 'pages/index.html'))
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+  //res.sendFile(path.resolve(__dirname,'pages/about.html'))
+  res.render("about");
 });
-
 app.get("/contact", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+  //res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+  res.render("contact");
 });
-
 app.get("/post", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/post.html"));
+  //res.sendFile(path.resolve(__dirname,'pages/post.html'))
+  res.render("post");
 });
 
 
