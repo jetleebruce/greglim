109f3dc552ba3ab497102d69f0bc42cccb3b71b2
Serge Modin <modinsv@yandex.ru>
2021-01-12 15:25:05 +0300

    Added nodemon, Page routes were created,
    We began our blog project with an existing blog template from
    startbootstrap.com and incorporated it into a Node.js project. Using the nodemon package, we automatically detect code changes in our project and restart the server. Page routes were created to serve the home, about and postpages. A navigation bar was created to contain the links to these routes.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require("express");
+const path = require("path");
 
 const app = new express();
+const ejs = require("ejs");
+app.set("view engine", "ejs");
+
 app.use(express.static("public"));
+
+app.get('/',(req,res)=>{
+res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+})
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+});
+
+app.get("/post", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/post.html"));
+});
 
 
 app.listen(4000, () => {
