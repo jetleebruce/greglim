7734a82986be01edecebd40bfc481acec9b8f8e8
Serge Modin <modinsv@yandex.ru>
2021-01-12 15:01:15 +0300

    Started new Project Blog
    Using some free Bootstrap template

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-const express = require("express"); // require express module
-const path = require("path");
+const express = require("express");
+
+const app = new express();
 app.use(express.static("public"));
-const app = express(); // calls express function to start new Express app
 
-app.listen(3000, () => {
-  console.log("App listening on port 3000");
+
+app.listen(4000, () => {
+  console.log("App listening on port 4000");
 });
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"));
-
-});
-
-app.get("/about", (req, res) => {
-  // called when request to /about comes in
-  res.sendFile(path.resolve(__dirname, "about.html"));
-});
-app.get("/contact", (req, res) => {
-  //called when request to /contact comes
-  res.sendFile(path.resolve(__dirname, "contact.html"));
-});
-
